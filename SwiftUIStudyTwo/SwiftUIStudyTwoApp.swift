48e89f2382bdeb46adf7df49d7628bb2acd54278
andysung <songzongjian0123@163.com>
2024-07-03 22:29:16 +0800

    first commit

---
+++ SwiftUIStudyTwo/SwiftUIStudyTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyTwoApp.swift
+//  SwiftUIStudyTwo
+//
+//  Created by andy on 2024/7/3.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
